83c241b74c8ebe9f035c1cc7111e07a5b7708308
mddejong <mjong@magnafacta.nl>
2023-09-06 16:19:10 +0200

    Added extra display options for smaller numeric input fields, Yes No Questions and Multiple choice with comments css hide-comment-inputs

--- sql/example-queries.sql
+++ sql/example-queries.sql
@@ -1,9 +1,16 @@
 
+SELECT template, COUNT(*) FROM ls__surveys GROUP BY template;
+
+SELECT template, surveyls_title FROM ls__surveys INNER JOIN ls__surveys_languagesettings ON sid = surveyls_survey_id WHERE template = 'gt-basic';
+
 -- Update the theme of all existing surveys
 UPDATE ls__surveys SET template = 'gt-basic';
 
 -- Update the theme of some existing surveys
 UPDATE ls__surveys SET template = 'gt-basic' WHERE template LIKE 'NetQure%';
+
+-- Update the theme of some existing surveys
+UPDATE ls__surveys SET template = 'gt-basic-kik' WHERE sid IN (SELECT surveyls_survey_id FROM ls__surveys_languagesettings WHERE surveyls_title LIKE 'KIK%');
 
 -- Allow users in all surveys to save and resume later 
 UPDATE ls__surveys SET allowsave = 'Y' WHERE allowsave = 'N';
